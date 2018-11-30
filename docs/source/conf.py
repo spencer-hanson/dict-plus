80bcaa9a31dad435b1b6f029817549e150b24bcf
Spencer Hanson <spencer.hanson@swimlane.com>
2018-11-30 14:37:09 -0700

    Updated docs again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -107,7 +107,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'DictionaryPlusdoc'
+htmlhelp_basename = 'DictionaryPlusDoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
