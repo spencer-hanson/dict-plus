b59158232e9ef5ecaf4a57b6b070cfd870b12783
Spencer Hanson <spencer.hanson@swimlane.com>
2019-07-17 15:22:37 -0600

    Version 0.0.7 - python2.7 fix/support

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 
 def parse_requirements(requirement_file):
-    with open(requirement_file) as fi:
-        return fi.readlines()
+    return ""
+    # Code doesn't work with python2.7 installation, just return the requirement hardcoded
+    # with open(requirement_file) as fi:
+    #     return fi.readlines()
 
 
 with open('./README.rst') as f:
@@ -12,12 +14,15 @@ with open('./README.rst') as f:
 setup(
     name='dict_plus',
     packages=['dict_plus'],
-    version='0.0.4',
+    version='0.0.7',
     description='Extended Dictionary Package',
     author='Spencer Hanson',
     author_email="spencerhanson3141@gmail.com",
     long_description=long_description,
-    install_requires=parse_requirements('./requirements.txt'),
+    install_requires=parse_requirements('requirements.txt'),
     keywords=['list', 'utilities', 'dictionary'],
-    classifiers=[],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ]
 )
